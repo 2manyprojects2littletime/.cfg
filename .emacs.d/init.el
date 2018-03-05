2eec60d3b317d7e2ce4e4ab1f2f2f8ef9704667a
Matt Duffy <matt@forestry.io>
2018-03-05 12:14:24 -0400

    Custom auto-config added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,21 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet web-mode use-package smex smartparens ruby-refactor robe projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flymake-ruby flycheck-cask expand-region exec-path-from-shell drag-stuff))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 
